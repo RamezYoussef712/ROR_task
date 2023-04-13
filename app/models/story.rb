5a1408bde9073720c6836151ba88029500741ca7
RamezYoussef712 <ramez.youssef.fahmy@gmail.com>
2023-04-13 23:39:28 +0200

    define top_stories scope

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,7 +2,8 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :reviews
 
-  validates :title, presence: true, length: { in: 6..40}
-  validates :body, presence: true, length: { minimum: 10}
+  validates :title, presence: true, length: { in: 6..40 }
+  validates :body, presence: true, length: { minimum: 10 }
 
+  scope :top_stories, -> { joins(:reviews).group(:id).order('AVG(reviews.rate) DESC ') }
 end
